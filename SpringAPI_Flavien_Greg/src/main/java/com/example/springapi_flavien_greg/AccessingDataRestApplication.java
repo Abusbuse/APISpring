d921256d6f1eb81962a6a219aaf13b2afbca3208
Wittmann Gregory <gregory.wittmann4@etu.univ-lorraine.fr>
2023-12-11 16:30:25 +0100

    Add all we need

---
+++ SpringAPI_Flavien_Greg/src/main/java/com/example/springapi_flavien_greg/AccessingDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapi_flavien_greg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataRestApplication.class, args);
+    }
+
+}
--- SpringAPI_Flavien_Greg/src/main/java/com/example/springapi_flavien_greg/UserRepository.java
+++ SpringAPI_Flavien_Greg/src/main/java/com/example/springapi_flavien_greg/UserRepository.java
@@ -2,8 +2,6 @@ package com.example.springapi_flavien_greg;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.accessingdatamysql.User;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
