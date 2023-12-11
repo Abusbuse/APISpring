661b0f85106c624c6e70720057bfd24f5140d755
Wittmann Gregory <gregory.wittmann4@etu.univ-lorraine.fr>
2023-12-11 14:46:51 +0100

    Add spring project

---
+++ SpringAPI_Flavien_Greg/src/main/java/com/example/springapi_flavien_greg/SpringApiFlavienGregApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapi_flavien_greg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiFlavienGregApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiFlavienGregApplication.class, args);
+    }
+
+}
